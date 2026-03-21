ba79123f4e3ef249be40e0a65e958e20ac6080c9
vikram(wsl) <vikramjit360@gmail.com>
2026-03-21 16:04:25 +0530

    feat: make changes to init Neon Postgres

---
+++ packages/database/src/client.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "./generated/client/index.js";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
