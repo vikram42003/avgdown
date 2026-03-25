24e1502c163a07959f66b11c2dbf612342cef4a0
vikram(wsl) <vikramjit360@gmail.com>
2026-03-25 19:08:55 +0530

    feat(api): install and configure winston-nestjs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            isProduction
+              ? winston.format.json()
+              : nestWinstonModuleUtilities.format.nestLike('AvgDown', {
+                  colors: true,
+                  prettyPrint: true,
+                }),
+          ),
+        }),
+      ],
+    }),
+  });
   await app.listen(process.env.PORT_API ?? 3001);
 }
 
