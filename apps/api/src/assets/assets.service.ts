46923d359843acc86a1ed42db26dc4db0ed29421
vikram(wsl) <vikramjit360@gmail.com>
2026-04-06 10:34:34 +0530

    feat(api): add routes for assets/ GET, GET :id, DELETE :id

--- apps/api/src/assets/assets.service.ts
+++ apps/api/src/assets/assets.service.ts
@@ -1,17 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException, Logger } from "@nestjs/common";
 import { AssetResponseDto } from "./assets.dto";
+import { PrismaService } from "src/common/database/prisma/prisma.service";
+import { prisma } from "@avgdown/db";
 
 @Injectable()
 export class AssetsService {
-  findAll(): Promise<AssetResponseDto[]> {
-    return `This action returns all assets`;
+  private readonly logger = new Logger(AssetsService.name);
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<AssetResponseDto[]> {
+    return await this.prismaService.asset.findMany();
   }
 
-  findOne(id: string): Promise<AssetResponseDto> {
-    return `This action returns a #${id} asset`;
+  async findOne(id: string): Promise<AssetResponseDto> {
+    const asset = await this.prismaService.asset.findUnique({ where: { id } });
+    if (asset === null) {
+      throw new NotFoundException(`Asset with id ${id} not found`);
+    }
+    return asset;
   }
 
-  remove(id: string): Promise<AssetResponseDto> {
-    return `This action removes a #${id} asset`;
+  async remove(id: string): Promise<AssetResponseDto> {
+    return await prisma.$transaction(async (tx) => {
+      const asset = await tx.asset.findUnique({ where: { id } });
+      if (asset === null) {
+        throw new NotFoundException(`Asset with id ${id} not found`);
+      }
+      return tx.asset.delete({ where: { id } });
+    });
   }
 }
