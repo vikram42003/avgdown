98ca2798a7101322fff7885eb8704fc18014d812
vikram(wsl) <vikramjit360@gmail.com>
2026-04-16 22:39:10 +0530

    infra: init and setup terraform, setup s3 remote backend

--- apps/worker/src/main.py
+++ apps/worker/src/main.py
@@ -4,7 +4,13 @@ from datetime import datetime, timezone
 from models import WatchlistEntryProjection, TriggeredAlert
 from collections import defaultdict
 
-from db import get_price_snapshots_bulk, get_recently_alerted_entries, add_price_snapshots_bulk, add_missed_fetch_bulk, get_watchlist_entries
+from db import (
+    get_price_snapshots_bulk,
+    get_recently_alerted_entries,
+    add_price_snapshots_bulk,
+    add_missed_fetch_bulk,
+    get_watchlist_entries,
+)
 from providers.yf import fetch_prices_bulk
 from utils import map_symbol_exchange, filter_inactive_markets
 from logic.sma import sma_val_below_average
@@ -68,15 +74,20 @@ def process_sma(
     SMA_VAL_BELOW_AVERAGE_DEVIATION_THRESHOLD = 0.02
 
     alerts_by_user: dict[str, dict[str, TriggeredAlert]] = defaultdict(dict)
-    
+
     sma_val_below_average(
-        entries_by_symbol, price_snapshots_by_asset_id, alerts_by_user, SMA_VAL_BELOW_AVERAGE_DEVIATION_THRESHOLD
+        entries_by_symbol,
+        price_snapshots_by_asset_id,
+        alerts_by_user,
+        SMA_VAL_BELOW_AVERAGE_DEVIATION_THRESHOLD,
     )
 
     return dict(alerts_by_user)
 
 
-if __name__ == "__main__":
+def lambda_handler(event, context):
+    return "THE LAMBDA IS ALIVE"
+
     # Fetch all watchlist entries (With joins on Assets and Users, since they'll be used next)
     watchlist_entries = get_watchlist_entries()
 
@@ -127,13 +138,13 @@ if __name__ == "__main__":
     if pending_entry_ids:
         # Ask DB which ones fired in the last 24 hours
         recently_alerted_ids = get_recently_alerted_entries(pending_entry_ids)
-        
+
         # Prune them from our dictionary so we don't send them again
         for user_id in list(alerts_by_user.keys()):
             for entry_id in recently_alerted_ids:
                 if entry_id in alerts_by_user[user_id]:
                     del alerts_by_user[user_id][entry_id]
-            
+
             # Clean up empty user dicts
             if not alerts_by_user[user_id]:
                 del alerts_by_user[user_id]
@@ -144,26 +155,8 @@ if __name__ == "__main__":
     flat_alerts_to_insert = []
     for user_alerts in alerts_by_user.values():
         flat_alerts_to_insert.extend(user_alerts.values())
-        
+
     add_alerts_bulk(flat_alerts_to_insert)
-"""
-Well, imagine we do have a watchlist in our db, now what the worker must do is
-fetch all the watchlist entries that are active first and like track all the assets we have to fetch right now
-now for that assetList, fetch the current price for each, and save that to the database
-SPECIAL CASE: If the fetch was unsuccessful then we put that into MissedFetch list
-Either way, we then fetch the last n snapshots of the price and then calculate the sma
 
-Now, for each watchlist entry, we check if the sma has crossed the trigger price
-If it has, we then check if the alert has already been sent
-If not, we send the alert and mark it as sent
-
-
-
-
-we have entries_by_asset[symbol] = [list of different users]
-now to see which period's sma we have to like query for, we find the largest sma period to find out
-how much data we have to fetch
-
-and then calculate the smas based on that for each user individually, its a math calculation so no need to batch it, it'll be quick as is
-after that we put that into alerts to send, and then send out alerts and then create the alert entries in the db in bulk
-"""
+if __name__ == "__main__":
+    lambda_handler({}, {})
